abc291e24bf12096d566d19fd9f8a88bd31dfb11
josemedina760 <josemedina760@gmail.com>
2015-10-29 03:07:46 -0700

    sleepy.... almost done

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,24 @@
 (function() {
-	'use strict';
-	angular.module('app', ['ui.router'])
-	.config(Config);
-	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
-	function Config($stateProvider, $urlRouterProvider) {
-		$stateProvider.state('Home',{
-			url: '/',
-			templateUrl: 'views/home.html'
-		});
-		$urlRouterProvider.otherwise('/');
-	}
+    'use strict';
+    angular.module('app', ['ui.router', 'uiGmapgoogle-maps'])
+        .config(Config);
+    Config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function Config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('Home', {
+            url: '/',
+            templateUrl: 'views/home.html'
+        }).state('About', {
+            url: '/About',
+            templateUrl: 'views/about.html'
+        }).state('Menu', {
+            url: '/Menu',
+            templateUrl: 'views/menu.html'
+        }).state('Contact', {
+            url: '/Contact_us',
+            templateUrl: 'views/contact.html',
+            controller: 'HomeController'
+        });
+        $urlRouterProvider.otherwise('/');
+    }
 })();
--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -1,12 +1,13 @@
 (function() {
-	'use strict';
-	angular.module('app')
-	.controller('HomeController', HomeController);
+    'use strict';
+    angular.module('app')
+        .controller('HomeController', HomeController);
 
-	HomeController.$inject = [];
+    HomeController.$inject = ['$scope', 'uiGmapGoogleMapApi'];
 
-	function HomeController() {
-		var vm = this;
-		vm.title = 'Welcome to my bakery shop website';
-	}
+    function HomeController($scope, uiGmapGoogleMapApi) {
+        var vm = this;
+        $scope.map = { center: { latitude: 37.808544, longitude: -122.253681 }, zoom: 16 };
+        $scope.marker = { coords: { latitude: 37.808544, longitude: -122.253681 }};
+    }
 })();
